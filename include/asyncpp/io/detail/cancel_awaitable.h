d959026da155db7da2544b3b95066dc8e4f48d51
Dominik Thalhammer <dominik@thalhammer.it>
2026-09-05 23:24:29 +0200

    :bug: Fix potential datarace in cancel_awaitable

--- include/asyncpp/io/detail/cancel_awaitable.h
+++ include/asyncpp/io/detail/cancel_awaitable.h
@@ -3,6 +3,7 @@
 #include <asyncpp/io/detail/io_engine.h>
 #include <asyncpp/stop_token.h>
 
+#include <mutex>
 #include <optional>
 
 namespace asyncpp::io::detail {
@@ -19,6 +20,7 @@ namespace asyncpp::io::detail {
 		T m_child;
 		asyncpp::stop_token m_stop_token;
 		std::optional<asyncpp::stop_callback<cancel_io_stop_callback>> m_cancel_callback;
+		std::mutex m_mtx;
 
 	public:
 		template<typename... Args>
@@ -26,6 +28,7 @@ namespace asyncpp::io::detail {
 			: m_child(std::forward<Args>(args)...), m_stop_token{std::move(st)} {}
 		bool await_ready() const noexcept { return m_child.await_ready(); }
 		bool await_suspend(coroutine_handle<> hdl) {
+			std::unique_lock lck(m_mtx);
 			if (m_stop_token.stop_requested()) {
 				m_child.m_completion.result = std::make_error_code(std::errc::operation_canceled);
 				return false;
@@ -36,6 +39,9 @@ namespace asyncpp::io::detail {
 					m_stop_token, cancel_io_stop_callback{&m_child.m_completion, m_child.m_socket.service().engine()});
 			return res;
 		}
-		auto await_resume() { return m_child.await_resume(); }
+		auto await_resume() {
+			std::unique_lock lck(m_mtx);
+			return m_child.await_resume();
+		}
 	};
 } // namespace asyncpp::io::detail
